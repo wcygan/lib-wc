599c3a2a48184e7006170e5b15e62e7f7f68e97a
Will Cygan <wcygan.io@gmail.com>
2022-11-17 06:57:45 -0800

    macro: hello

--- src/macros/basic_macros.rs
+++ src/macros/basic_macros.rs
@@ -7,17 +7,38 @@ macro_rules! add{
     }
 }
 
+#[allow(unused_macros)]
+macro_rules! hello{
+    ($a:expr)=>{
+        {
+            String::from("hello ") + $a
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn add_numbers() {
-        let x = add!(1, 2);
-        assert_eq!(x, 3)
+        let result = add!(1, 2);
+        assert_eq!(result, 3)
     }
 
     #[test]
     fn add_strings() {
-        let x = add!("hello ".to_owned(), "world");
-        assert_eq!(x, "hello world")
+        let result = add!("hello ".to_owned(), "world");
+        assert_eq!(result, "hello world")
+    }
+
+    #[test]
+    fn hello_world() {
+        let result = hello!("world");
+        assert_eq!(result, "hello world")
+    }
+
+    #[test]
+    fn hello_number() {
+        let result = hello!("123");
+        assert_eq!(result, "hello 123")
     }
 }
