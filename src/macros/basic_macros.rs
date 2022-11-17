6a7f0d17f4dc9aafc3ad66e8c178cf80f53c2c21
Will Cygan <wcygan.io@gmail.com>
2022-11-16 19:50:22 -0800

    macros: add

---
+++ src/macros/basic_macros.rs
@@ -0,0 +1,22 @@
+macro_rules! add{
+    ($a:expr,$b:expr)=>{
+        {
+            $a+$b
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn add_numbers() {
+        let x = add!(1, 2);
+        assert_eq!(x, 3)
+    }
+
+    #[test]
+    fn add_strings() {
+        let x = add!("hello ".to_owned(), "world");
+        assert_eq!(x, "hello world")
+    }
+}
