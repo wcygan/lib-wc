8c100f865aff86b92bef285989178c22cdd62eca
Will Cygan <wcygan.io@gmail.com>
2022-11-20 19:53:23 -0800

    add tests for tree

--- src/data_structures/trees/bst/mod.rs
+++ src/data_structures/trees/bst/mod.rs
@@ -57,4 +57,75 @@ mod tests {
     fn default() {
         let t = Tree::<u32>::default();
     }
+
+    #[test]
+    fn test_minimum() {
+        let mut t = Tree::<u32>::default();
+
+        t.insert(1);
+        t.insert(2);
+        t.insert(3);
+
+        match t.minimum() {
+            None => {
+                panic!("should not hit this branch")
+            }
+            Some(val) => {
+                assert_eq!(1, *val)
+            }
+        }
+    }
+
+    #[test]
+    fn test_maximum() {
+        let mut t = Tree::<u32>::default();
+
+        t.insert(1);
+        t.insert(2);
+        t.insert(3);
+
+        match t.maximum() {
+            None => {
+                panic!("should not hit this branch")
+            }
+            Some(val) => {
+                assert_eq!(3, *val)
+            }
+        }
+    }
+
+    #[test]
+    fn search_for_value_in_tree() {
+        let mut t = Tree::<u32>::default();
+
+        t.insert(1);
+
+        assert_eq!(t.search(&1), true)
+    }
+
+    #[test]
+    fn search_for_value_not_in_tree() {
+        let mut t = Tree::<u32>::default();
+
+        assert_eq!(t.search(&1), false)
+    }
+
+    #[test]
+    fn insert_does_not_panic() {
+        let mut t = Tree::<u32>::default();
+
+        for i in 0..10 {
+            t.insert(i)
+        }
+    }
+
+    #[test]
+    fn search_does_not_panic() {
+        let mut t = Tree::<u32>::default();
+
+        for i in 0..10 {
+            t.insert(i);
+            t.search(&i);
+        }
+    }
 }
