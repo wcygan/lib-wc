ffd1b69500d0e34d73faf4adabe71831933deb7e
Will Cygan <wcygan.io@gmail.com>
2022-11-25 10:33:17 -1000

    scoped threads

--- src/language_features/threads/fun_with_threads.rs
+++ src/language_features/threads/fun_with_threads.rs
@@ -1,12 +1,33 @@
 #[cfg(test)]
 mod tests {
+    use std::thread;
 
     #[test]
     fn spawn_a_thread() {
-        let t = std::thread::spawn(|| {
+        let t = thread::spawn(|| {
             println!("Hello from a thread!");
         });
 
         t.join().unwrap()
+    }
+
+    #[test]
+    fn scoped_threads () {
+        // thread::scope spawns "scoped" threads that cannot outlive the scope of the
+        // closures that we pass to it.
+        // scopes allow us to safely borrow data from the parent thread
+        let numbers = vec![1, 2, 3];
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                println!("length: {}", numbers.len());
+            });
+
+            s.spawn(|| {
+                for n in &numbers {
+                    println!("{n}");
+                }
+            });
+        });
     }
 }
