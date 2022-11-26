26fa2dfe8c756e1f2835b38222c577f6be96abe6
Will Cygan <wcygan.io@gmail.com>
2022-11-25 17:03:58 -1000

    scoped counter

--- src/language_features/atomics/fun_with_atomics.rs
+++ src/language_features/atomics/fun_with_atomics.rs
@@ -1,7 +1,11 @@
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
-    use std::sync::atomic::AtomicBool;
+    use std::sync::atomic::{AtomicBool, AtomicUsize};
+    use std::sync::atomic::Ordering::Relaxed;
+    use std::thread;
+    use std::thread::sleep;
+    use std::time::Duration;
     use crate::concurrent::executors::multi_threaded::ThreadPool;
 
     #[test]
@@ -13,12 +17,37 @@ mod tests {
         pool.execute({
             let val = val.clone();
             move || {
-                val.store(true, std::sync::atomic::Ordering::Relaxed);
+                val.store(true, Relaxed);
             }
         });
 
         drop(pool);
 
-        assert_eq!(val.load(std::sync::atomic::Ordering::Relaxed), true);
+        assert_eq!(val.load(Relaxed), true);
+    }
+
+    #[test]
+    fn counter_progress() {
+        let num_done = AtomicUsize::new(0);
+
+        thread::scope(|s| {
+            // A background thread to process all 100 items.
+            s.spawn(|| {
+                for i in 0..100 {
+                    sleep(Duration::from_millis(2));
+                    num_done.store(i + 1, Relaxed);
+                }
+            });
+
+            // The main thread shows status updates, every second.
+            loop {
+                let n = num_done.load(Relaxed);
+                if n == 100 { break; }
+                println!("Working.. {n}/100 done");
+                sleep(Duration::from_millis(50));
+            }
+        });
+
+        println!("Done!");
     }
 }
