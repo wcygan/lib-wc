825854e4a1d36f75426097f6d2ddcbe266a06dbb
Will Cygan <wcygan.io@gmail.com>
2022-12-05 22:13:14 -0800

    tidy up benchmarking conventions

--- benches/src/concurrent/locks/mod.rs
+++ benches/src/concurrent/locks/mod.rs
@@ -5,10 +5,10 @@ static ITERATIONS: usize = 100_000;
 // This will benchmark a lock under no contention
 // This means that the current thread is the only thread attempting to acquire the lock
 macro_rules! lock_uncontended(
-    ($name: ident, $lockType: ty) => {
-        fn $name(bh: &mut criterion::Criterion) {
-            bh.bench_function(stringify!($name), move |bh| bh.iter(|| {
-                let lock = <$lockType>::new(0);
+    ($fn_name: ident, $T: ty) => {
+        fn $fn_name(bh: &mut criterion::Criterion) {
+            bh.bench_function(stringify!($fn_name), move |bh| bh.iter(|| {
+                let lock = <$T>::new(0);
                 for _ in 0..ITERATIONS {
                     *lock.lock() += 1;
                 }
@@ -20,10 +20,10 @@ macro_rules! lock_uncontended(
 // This will benchmark a lock under contention
 // This means that multiple threads are attempting to acquire the lock concurrently
 macro_rules! lock_with_contention(
-    ($name: ident, $lockType: ty) => {
-        fn $name(bh: &mut criterion::Criterion) {
-            bh.bench_function(stringify!($name), move |bh| bh.iter(|| {
-                let lock = <$lockType>::new(0);
+    ($fn_name: ident, $T: ty) => {
+        fn $fn_name(bh: &mut criterion::Criterion) {
+            bh.bench_function(stringify!($fn_name), move |bh| bh.iter(|| {
+                let lock = <$T>::new(0);
                 std::thread::scope(|s| {
                     for _ in 0..16 {
                         s.spawn(|| {
