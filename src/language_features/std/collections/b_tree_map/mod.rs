35a487aa655464840b765fa96d8a26546f2ac042
Will Cygan <wcygan.io@gmail.com>
2022-12-13 20:16:33 -0800

    more btree tests

--- src/language_features/std/collections/b_tree_map/mod.rs
+++ src/language_features/std/collections/b_tree_map/mod.rs
@@ -9,4 +9,41 @@ mod tests {
         t.insert(1, 2);
         assert_eq!(true, t.contains_key(&1));
     }
+
+    #[test]
+    fn btree_five() {
+        let mut t: BTreeMap<usize, usize> = BTreeMap::new();
+        t.insert(1, 2);
+        t.insert(2, 3);
+        t.insert(3, 4);
+        t.insert(4, 5);
+        t.insert(5, 6);
+        assert_eq!(Some(&2), t.get(&1));
+        assert_eq!(Some(&3), t.get(&2));
+        assert_eq!(Some(&4), t.get(&3));
+        assert_eq!(Some(&5), t.get(&4));
+        assert_eq!(Some(&6), t.get(&5));
+    }
+
+    #[test]
+    fn btree_min() {
+        let mut t: BTreeMap<usize, usize> = BTreeMap::new();
+        t.insert(1, 2);
+        t.insert(2, 3);
+        t.insert(3, 4);
+        t.insert(4, 5);
+        t.insert(5, 6);
+        assert_eq!(Some((&1, &2)), t.iter().next());
+    }
+
+    #[test]
+    fn btree_max() {
+        let mut t: BTreeMap<usize, usize> = BTreeMap::new();
+        t.insert(1, 2);
+        t.insert(2, 3);
+        t.insert(3, 4);
+        t.insert(4, 5);
+        t.insert(5, 6);
+        assert_eq!(Some((&5, &6)), t.iter().next_back());
+    }
 }
