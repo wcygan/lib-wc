e2f500ba18a506012b80cf8bb95863245dce06dc
Will Cygan <wcygan.io@gmail.com>
2023-01-02 13:56:15 -0600

    add threadpool_shutdown example

---
+++ examples/threadpool_shutdown.rs
@@ -0,0 +1,24 @@
+use std::sync::atomic::AtomicUsize;
+
+use lib_wc::concurrent::executors::thread_pool::{RayonThreadPool, ThreadPool, ThreadPoolError};
+use std::sync::atomic::Ordering::Acquire;
+
+fn main() -> Result<(), ThreadPoolError> {
+    static COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    let pool = RayonThreadPool::new(2)?;
+
+    pool.spawn(|| println!("All jobs should run before the program exits!"));
+
+    for _ in 0..100 {
+        pool.spawn(move || {
+            COUNT.fetch_add(1, Acquire);
+        });
+    }
+
+    pool.shutdown();
+
+    assert_eq!(COUNT.load(Acquire), 100);
+
+    Ok(())
+}
