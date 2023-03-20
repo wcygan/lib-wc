7c7a781691f85317347481797b47713f46590f29
Will Cygan <wcygan.io@gmail.com>
2023-03-19 19:43:15 -0700

    Tokio stdin interrupt

---
+++ experiments/tokio-stdin-interrupt/readme.md
@@ -0,0 +1,7 @@
+# Tokio stdin interrupt
+
+Why?
+
+There is no great way to wait for stdin input (e.g., keyboard input) asynchronously.
+
+This experiment aims to find a way to do it nicely.
---
+++ experiments/tokio-stdin-interrupt/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, BufRead, BufReader};
+use tokio::select;
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<String>(10);
+    std::thread::spawn(move || get_input(tx));
+
+    select! {
+        _ = tokio::signal::ctrl_c() => {}
+        line = rx.recv() => {
+            match line {
+                Some(s) => println!("{s}"),
+                None => {}
+            }
+        }
+    }
+}
+
+fn get_input(tx: mpsc::Sender<String>) {
+    loop {
+        let reader = BufReader::new(stdin());
+        let mut lines = reader.lines();
+        if let Some(r) = lines.next() {
+            if let Ok(s) = r {
+                let _ = tx.blocking_send(s);
+            }
+        }
+    }
+}
